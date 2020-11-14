4d7812ca8ed2c0e2d68a220c3250b65ef53831bf
wareewan-opal <68785361+wareewan-opal@users.noreply.github.com>
2020-11-14 23:36:40 +0700

    -add new page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import '../node_modules/aos/dist/aos.css';
 import './App.css';
 import Menu from './components/Menu';
-import Home from './components/Home'
+import Home from './components/Home';
+import Legend from './components/Legend';
 function App() {
 
   return (
     <div className="App">
         <Menu/>
         <Home/>
+        <Legend/>
     </div>
   );
 }
---
+++ src/components/Legend.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../style/miss.css';
+import missed from '../images/missed.png';
+import Lampcol from '../images/Lampcol.png';
+class Legend extends Component {
+    render() {
+        return (
+            <div className="missed">
+                <div>
+                <h1 className="miss-lang">ฉางเอ๋อ <span className="mis-lang-lang">(嫦娥)</span></h1>
+                <img src={missed} id="miss" />
+                </div>
+                <img src={Lampcol} id="lamp" />
+                    <p>ตำนานของเทศกาลไหว้พระจันทร์</p>
+                    <p className="write">เกี่ยวกับเทพปกรณัมจีนที่เล่าถึงเทพธิดาแห่งดวงจันทร์ ที่ชื่อ “ฉางเอ๋อ”
+                    ซึ่งเป็นหญิงคนรักของ โฮวอี้ นักยิงธนูแห่งสวรรค์
+                    ที่ใช้ธนูยิงดวงอาทิตย์ตกลงไปถึง 9 ดวงจากทั้งหมด 10 ดวง
+                    ซึ่งเป็นการกระทำที่ฝ่าฝืนบัญชาสวรรค์จึงโดนลงทัณฑ์ให้ไปใช้ชีวิตธรรมดา
+                    เช่น มนุษย์ทั่วไปบนโลกมนุษย์กับฉางเอ๋อ แต่แล้วโฮวอี้ก็ถูกคนสนิททรยศฆ่าตาย
+                    ส่วนฉางเอ๋อนางได้ดื่มน้ำอมฤตเพื่อที่จะมีชีวิตอมตะ
+                    แล้วเหาะกลับไปยังดวงจันทร์อีกครั้งตามลำพังด้วยความเศร้า</p>
+                    <hr></hr>
+                </div>
+            
+        )
+    }
+}
+export default Legend;
