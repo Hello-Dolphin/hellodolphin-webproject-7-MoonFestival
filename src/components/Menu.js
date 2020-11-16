35c4b9cfbb08c4b472c800e851f0f3f5fb4cc44c
wareewan-opal <68785361+wareewan-opal@users.noreply.github.com>
2020-11-17 00:19:34 +0700

    -change picture

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,8 +9,8 @@ class Menu extends Component{
         return(
             <nav className="clearfix">
                 <ul>
-                    <li><a href="">หน้าแรก</a></li>
-                    <li><a href="">ตำนานฉางเอ๋อ</a></li>
+                    <li><a href="#">หน้าแรก</a></li>
+                    <li><a href="#">ตำนานฉางเอ๋อ</a></li>
                     <li><a href="#">วันไหว้พระจันทร์</a></li>
                     <li><a href="#"><img src={MenuLogo}/></a></li>
                     <li><a href="#">ขนมไหว้พระจันทร์</a></li>
