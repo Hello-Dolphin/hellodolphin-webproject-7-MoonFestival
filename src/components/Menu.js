2b462bec06e116c8322e43a3ecb43ca55719dfe0
wareewan-opal <68785361+wareewan-opal@users.noreply.github.com>
2020-11-17 22:48:04 +0700

    -update version

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,15 +9,6 @@ class Menu extends Component{
         return(
             <nav className="clearfix">
                 <ul>
-<<<<<<< HEAD
-                    <li><a href="#">หน้าแรก</a></li>
-                    <li><a href="#">ตำนานฉางเอ๋อ</a></li>
-                    <li><a href="#">วันไหว้พระจันทร์</a></li>
-                    <li><a href="#"><img src={MenuLogo}/></a></li>
-                    <li><a href="#">ขนมไหว้พระจันทร์</a></li>
-                    <li><a href="#">พิธี</a></li>
-                    <li><a href="#">กระต่ายบนจันทร์</a></li>
-=======
                     <li><a href="#home">หน้าแรก</a></li>
                     <li><a href="#legend">ตำนานฉางเอ๋อ</a></li>
                     <li><a href="#moonday">วันไหว้พระจันทร์</a></li>
@@ -25,7 +16,6 @@ class Menu extends Component{
                     <li><a href="#mooncake-page">ขนมไหว้พระจันทร์</a></li>
                     <li><a href="#festival">พิธี</a></li>
                     <li><a href="#rabbit">กระต่ายบนจันทร์</a></li>
->>>>>>> 58806a8a759fb8a1b7a41ae0a36e20df6ca36042
                 </ul>
             </nav>
         );
